1e895c201b69a4d69b28de2019e8eade7600a8b6
dariv <davidrivet@hotmail.fr>
2018-04-02 23:22:07 +0200

    all current files added, need to complete xml and mock-only files

---
+++ json-server-xml.js
@@ -0,0 +1,11 @@
+// json-server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('file.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(4000, () => {
+  console.log('JSON Server is running')
+})
